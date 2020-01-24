ac379b55d29b853b4aea5a3a61ba60cb6616b43b
chapz <germainchi43@gmail.com>
2020-01-24 01:09:40 -0600

    JS file

--- script.js
+++ script.js
@@ -4,8 +4,46 @@ function update() {
   }
   
   setInterval(update, 1000);
+  
+  var store5 = $("#input5");
+  var renderText = $("#store");
 
 
+    renderLastRegistered();
+
+    function renderLastRegistered() {
+        var store5 = localStorage.getItem('id');
+      
+        if (store5 === null) {
+          return;
+        }
+      
+        renderText.attr("value", store5);
+        
+      }
+
+    $('#btn5').on('click', function(event){
+        event.preventDefault();
+       
+        var inputEl = $('#input5').val();
+        $('#input5').text(inputEl);
+        console.log(inputEl);
+
+        $('input[type="text"]').each(function(){   
+            $(this).addClass('red'); 
+            var id = $(this).attr('id');
+            var value = $(this).val();
+           localStorage.setItem(id, value);
+           renderLastRegistered();
+           
+           
+    
+        });           
+
+
+    });
+    
+    
 
 
 
